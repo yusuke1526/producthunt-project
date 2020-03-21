e7941e5c141632b6a7c012849e81b8efd0906daf
Yusuke Ueda <418810@m.mie-u.ac.jp>
2020-03-22 00:23:32 +0900

    only once vote and sorted by votes

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Vote
 
 admin.site.register(Product)
+admin.site.register(Vote)
--- products/views.py
+++ products/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Vote
 from django.utils import timezone
 
 def home(request):
-    products = Product.objects
-    return render(request, 'products/home.html', {'products': products})
+    products = Product.objects.all().order_by('-votes_total')
+    # packing products and voted
+    voted = []
+    for product in products:
+        if Vote.exists(product_id=product.id, user_id=request.user.id):
+            voted.append(True)
+        else:
+            voted.append(False)
+    products_and_voted = []
+    for i in range(len(products)):
+        products_and_voted.append({
+            'product': products[i],
+            'voted': voted[i],
+        })
+    return render(request, 'products/home.html', {'products_and_voted': products_and_voted})
 
 @login_required
 def create(request):
@@ -32,12 +45,24 @@ def create(request):
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     # pk is primary key
-    return render(request, 'products/detail.html', {'product': product})
+    exists_vote = Vote.exists(product_id=product_id, user_id=request.user.id)
+    return render(request, 'products/detail.html', {'product': product, 'exists_vote': exists_vote})
 
 @login_required
 def upvote(request, product_id):
     if request.method == 'POST':
-        product = get_object_or_404(Product, pk=product_id)
-        product.votes_total += 1
-        product.save()
-        return redirect('/products/' + str(product.id))
+        # check the existanse of vote
+        user_id = request.user.id
+        try:
+            vote = Vote.objects.get(product_id=product_id, user_id=user_id)
+            pass
+        except Vote.DoesNotExist:
+            # make vote
+            vote = Vote()
+            vote.product_id = product_id
+            vote.user_id = user_id
+            vote.save()
+            product = get_object_or_404(Product, pk=product_id)
+            product.votes_total += 1
+            product.save()
+        return redirect('/products/' + str(product_id))
